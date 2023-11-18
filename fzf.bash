d9742f8175daea20b74f4a7eafd1145b35c6d5bc
JZ Loh <ljzyx86@qq.com>
2023-11-18 15:25:51 +0800

    add fzf as submodule

--- fzf.bash
+++ fzf.bash
@@ -1,15 +1,15 @@
 # Setup fzf
 if [[ ! "$PATH" == *fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
+  PATH="${PATH:+${PATH}:}$HOME/.config/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.config/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.bash"
+source "$HOME/.config/fzf/shell/key-bindings.bash"
 unset -f __fzf_history__
 
 EXCLUDE_DIRS=(\
