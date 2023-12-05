f9da77ce3e1a6bc7a77e2894f5bd0194dfd0f091
JZ Loh <ljzyx86@qq.com>
2023-12-05 17:37:15 +0800

    modify keybind of Resize with arrows and telescope theme

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,7 +20,7 @@ local keymap = vim.api.nvim_set_keymap
 keymap("n", "<leader>w", ":w<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<A-Up>", ":resize -2<CR>", opts)
-keymap("n", "<A-Down>", ":resize +2<CR>", opts)
-keymap("n", "<A-Left>", ":vertical resize +2<CR>", opts)
-keymap("n", "<A-Right>", ":vertical resize -2<CR>", opts)
+keymap("n", "<A-Up>", ":resize +2<CR>", opts)
+keymap("n", "<A-Down>", ":resize -2<CR>", opts)
+keymap("n", "<A-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<A-Right>", ":vertical resize +2<CR>", opts)
