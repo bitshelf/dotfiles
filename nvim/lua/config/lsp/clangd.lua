c6b2fab6bc89e1e42507f3766907bdfe3e51ca86
JZ Loh <ljzyx86@qq.com>
2023-12-06 14:37:41 +0800

    update clangd config

--- nvim/lua/config/lsp/clangd.lua
+++ nvim/lua/config/lsp/clangd.lua
@@ -9,6 +9,11 @@ return {
 			  "--header-insertion=never",
 			  "--all-scopes-completion",
 			  "--completion-style=detailed",
+			  "--pch-storage=memory",
+			  -- 启用这项时，补全函数时，将会给参数提供占位符，键入后按 Tab 可以切换到下一占位符
+			  "--function-arg-placeholders=false",
+			  "--log=verbose",
+			  "-j=20",
 			};
 			filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto","dts", },
 		})
