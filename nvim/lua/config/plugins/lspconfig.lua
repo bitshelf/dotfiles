296798c07e8a36aef313d613e8923611a5607af3
JZ Loh <luojianzhi@industio.com>
2024-01-01 00:14:31 +0800

    add java lsp

--- nvim/lua/config/plugins/lspconfig.lua
+++ nvim/lua/config/plugins/lspconfig.lua
@@ -23,6 +23,7 @@ M.config = {
 				},
 			},
 			{ 'neovim/nvim-lspconfig' },
+			{ 'mfussenegger/nvim-jdtls' },
 			{
 				'williamboman/mason.nvim',
 				build = function()
@@ -59,6 +60,7 @@ M.config = {
 				-- 'pyright',
 				--'luau_lsp',
 				'bashls',
+				'jdtls',
 				'rust_analyzer'
 			  },
 			 handlers = {
