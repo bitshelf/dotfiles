5148f4e82251a63cb9c7c5ed2531492792933d75
JZ Loh <luojianzhi@industio.com>
2024-01-01 15:17:56 +0800

    remove automatically change root directory

--- nvim/lua/config/plugins/project.lua
+++ nvim/lua/config/plugins/project.lua
@@ -1,10 +1,13 @@
 return {
   "ahmedkhalf/project.nvim",
     config = function()
+	local opts = {silent = true, nowait = true}
+	vim.keymap.set('n', '<leader>..', ':ProjectRoot<cr>', opts)
+
     require("project_nvim").setup {
 		-- Manual mode doesn't automatically change your root directory, so you have
 		-- the option to manually do so using `:ProjectRoot` command.
-		manual_mode = false,
+		manual_mode = true,
 
 		-- Methods of detecting the root directory. **"lsp"** uses the native neovim
 		-- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
@@ -25,7 +28,7 @@ return {
 		exclude_dirs = {},
 
 		-- Show hidden files in telescope
-		show_hidden = false,
+		show_hidden = true,
 
 		-- When set to false, you will get a message when project.nvim changes your
 		-- directory.
