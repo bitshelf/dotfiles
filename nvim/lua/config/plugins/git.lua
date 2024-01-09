16dd28f6bbef1a1f3088df94c22cda585499bf40
JZ Loh <ljzyx86@qq.com>
2024-01-09 17:52:44 +0800

    add lazygit plugin depend

--- nvim/lua/config/plugins/git.lua
+++ nvim/lua/config/plugins/git.lua
@@ -22,7 +22,11 @@ return {
 	},
 	{
 		"kdheepak/lazygit.nvim",
+		dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
 		keys = {"<leader>gg"},
+		-- enabled = false,
 		config = function()
 			vim.g.lazygit_floating_window_scaling_factor = 1.0
 			vim.g.lazygit_floating_window_winblend = 0
