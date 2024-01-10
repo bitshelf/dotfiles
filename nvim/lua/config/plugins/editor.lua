d2829888de63bf515f6f338da87df91e6b3f6724
JZ Loh <ljzyx86@qq.com>
2024-01-10 17:37:39 +0800

    add nvim-spectre  replace plugin

--- nvim/lua/config/plugins/editor.lua
+++ nvim/lua/config/plugins/editor.lua
@@ -26,6 +26,15 @@ return {
 		end
 	},
 	{
+      "nvim-pack/nvim-spectre",                                                                                                                                               build = false,
+      cmd = "Spectre",
+      opts = { open_cmd = "noswapfile vnew" },
+      -- stylua: ignore
+      keys = {
+        { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+      },
+    },
+	{
 		"NvChad/nvim-colorizer.lua",
 		opts = {
 			filetypes = { "*" },
