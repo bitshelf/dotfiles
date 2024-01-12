7533e4f73e7baa0337d3c05a17399f2ca8f6dc63
JZ Loh <ljzyx86@qq.com>
2024-01-12 15:50:23 +0800

    add clangd lsp support

---
+++ nvim/lua/plugins/clangd.lua
@@ -0,0 +1,26 @@
+return {
+	"neovim/nvim-lspconfig",
+	opts = {
+		setup = {
+			clangd = function(_, opts)
+				opts.cmd = {
+					"clangd",
+					"--background-index",
+					-- "--clang-tidy",
+					"--header-insertion=iwyu",
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--fallback-style=llvm",
+					"--header-insertion=never",
+					"--all-scopes-completion",
+					"--completion-style=detailed",
+					"--offset-encoding=utf-16",
+					"--pch-storage=memory",
+					-- 启用这项时，补全函数时，将会给参数提供占位符，键入后按 Tab 可以切换到下一占位符
+					"--function-arg-placeholders=false",
+					"-j=20",
+				}
+			end,
+		},
+	},
+}
