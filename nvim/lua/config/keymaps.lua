5ce27d9852e661546429de5cdff2f1af506b5f56
JZ Loh <ljzyx86@qq.com>
2024-01-13 02:03:36 +0800

    modify keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+vim.keymap.set('n', "<F2>", ":nohlsearch<CR>", { silent = true, desc = "nohlsearch" })
+vim.keymap.set('x', "ZQ", "<Esc>:quit<CR>", { silent = true, desc = "visual quit not save" })
+vim.keymap.set('x', "ZZ", "<Esc>:wqa<CR>", { silent = true ,desc = "visual quit" })
