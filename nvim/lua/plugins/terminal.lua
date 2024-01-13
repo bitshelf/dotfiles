548ac333068a057591637c72dccf912d8308caef
JZ Loh <luojianzhi@industio.com>
2024-01-13 10:20:13 +0800

    add terminal plugin

---
+++ nvim/lua/plugins/terminal.lua
@@ -0,0 +1,23 @@
+return {
+  'akinsho/toggleterm.nvim', 
+  event = "LazyFile",
+  -- enabled = false,
+  opts = {
+	  open_mapping = [[<c-\>]],
+	  direction = 'float',
+	  close_on_exit = true, -- close the terminal window when the process exits
+	   -- Change the default shell. Can be a string or a function returning a string
+	  shell = vim.o.shell,
+	  auto_scroll = true, -- automatically scroll to the bottom on terminal output
+	  -- This field is only relevant if direction is set to 'float'
+	  float_opts = {
+		-- The border key is *almost* the same as 'nvim_open_win'
+		-- see :h nvim_open_win for details on borders however
+		-- the 'curved' border is a custom border type
+		-- not natively supported but implemented in this plugin.
+		border = 'single',
+		-- width = 100,
+		height = 80,
+	  },
+  },
+}
