8a2d12d043098ee1399b9449a17a2a6bfa7d9a20
JZ Loh <luojianzhi@industio.com>
2024-01-14 20:47:25 +0800

    add flash under the cursor keybind

--- nvim/lua/config/plugins/jump.lua
+++ nvim/lua/config/plugins/jump.lua
@@ -43,6 +43,7 @@ return {
 	    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
 	    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 	    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	    { "<leader>sd", mode = { "n", "o", "x" }, function() require("flash").jump({ pattern = vim.fn.expand("<cword>"), })end, desc = "flash cursor" },
 		{ "<leader>gl", mode = { "n", "o", "x" }, function() require("flash").jump({ search = { mode = "search", max_length = 0 }, label = { after = { 0, 0 } }, pattern = "^"}) end, desc = "goto line" },
 		{ "tt",mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter",},
 	  },
