01ae5c0abcebe64894015cea52c797310defec3a
JZ Loh <luojianzhi@industio.com>
2024-01-14 12:53:05 +0800

    add nvim-docs-view plugin

--- nvim/lua/config/plugins/coding.lua
+++ nvim/lua/config/plugins/coding.lua
@@ -6,4 +6,16 @@ return {
 	  vim.g.matchup_matchparen_offscreen = { method = "popup" }
 	end,
   },
+
+  {
+	"amrbashir/nvim-docs-view",
+	event = "VimEnter",
+
+	lazy = true,
+	cmd = "DocsViewToggle",
+	opts = {
+	  position = "right",
+	  width = 60
+	}
+  }
 }
