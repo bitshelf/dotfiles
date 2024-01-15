46872e4ea0f465f4cb03e807e6cf282ece2c66a2
JZ Loh <luojianzhi@industio.com>
2024-01-15 23:52:44 +0800

    modify ZQ keybind

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@ local opts = { noremap = true, silent = true, nowait = true }
 local keymap = vim.keymap.set
 
 vim.keymap.set('n', "<F2>", ":nohlsearch<CR>", { silent = true, desc = "nohlsearch" })
-vim.keymap.set('x', "ZQ", "<Esc>:quit<CR>", { silent = true, desc = "visual quit not save" })
-vim.keymap.set('x', "ZZ", "<Esc>:wqa<CR>", { silent = true ,desc = "visual quit" })
+vim.keymap.set({'n','x'}, "ZQ", "<Esc>:quitall<CR>", { silent = true, desc = "visual quit not save" })
+vim.keymap.set({'n','x'}, "ZZ", "<Esc>:wqa<CR>", { silent = true ,desc = "visual quit" })
