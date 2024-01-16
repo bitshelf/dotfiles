d664d7d6566aa874c1ca2c6839e308519c019976
JZ Loh <ljzyx86@qq.com>
2024-01-16 09:54:09 +0800

    del keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,3 +4,5 @@ local keymap = vim.keymap.set
 vim.keymap.set('n', "<F2>", ":nohlsearch<CR>", { silent = true, desc = "nohlsearch" })
 vim.keymap.set({'n','x'}, "ZQ", "<Esc>:quitall<CR>", { silent = true, desc = "visual quit not save" })
 vim.keymap.set({'n','x'}, "ZZ", "<Esc>:wqa<CR>", { silent = true ,desc = "visual quit" })
+
+vim.keymap.del('n',"<leader>e")
