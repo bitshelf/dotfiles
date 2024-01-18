e4301fb54c2ec8a16cccc5008ffceabd32675b3b
JZ Loh <luojianzhi@industio.com>
2024-01-19 00:12:58 +0800

    add rust lang lsp support

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
 	-- { import = "lazyvim.plugins.extras.coding.yanky" },
 	-- { import = "lazyvim.plugins.extras.util.project" },
 	{ import = "lazyvim.plugins.extras.editor.symbols-outline" },
