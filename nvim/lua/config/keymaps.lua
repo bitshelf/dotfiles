2fc9632399b472fe4c3265f02b60e093e0063814
JZ Loh <ljzyx86@qq.com>
2024-01-18 17:14:19 +0800

    remove lazyvim terminal keybind

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,8 +4,14 @@ local keymap = vim.keymap.set
 vim.keymap.set('n', "<F2>", ":nohlsearch<CR>", { silent = true, desc = "nohlsearch" })
 vim.keymap.set({'n','x'}, "ZQ", "<Esc>:quitall<CR>", { silent = true, desc = "visual quit not save" })
 vim.keymap.set({'n','x'}, "ZZ", "<Esc>:wqa<CR>", { silent = true ,desc = "visual quit" })
+vim.keymap.set('n', '<leader>l', "<cmd>UndotreeToggle<cr>", { desc = "UndotreeToggle", silent = true, nowait = true })
 
 -- vim.keymap.del('n',"<leader>e")
-vim.keymap.del('n',"<leader>L")
-vim.keymap.del('n',"<leader>l")
-vim.keymap.set('n', '<leader>l', "<cmd>UndotreeToggle<cr>", { desc = "UndotreeToggle", silent = true, nowait = true })
+vim.keymap.del('n', "<leader>L")
+vim.keymap.del('n', "<leader>l")
+
+-- remove lazyvim terminal keybind
+vim.keymap.set("t", "<c-h>", "<c-h>", { buffer = buf, nowait = true })
+vim.keymap.set("t", "<c-j>", "<c-j>", { buffer = buf, nowait = true })
+vim.keymap.set("t", "<c-k>", "<c-k>", { buffer = buf, nowait = true })
+vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = buf, nowait = true })
