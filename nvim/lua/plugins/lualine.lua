bed2de780e75645044e3e17cf3ec3edb2774b6b7
JZ Loh <ljzyx86@qq.com>
2024-01-24 18:38:15 +0800

    enabled file name in lualine status line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
-      table.remove(opts.sections.lualine_c)
+      -- table.remove(opts.sections.lualine_c)
       table.remove(opts.sections.lualine_z)
     end,
   }
