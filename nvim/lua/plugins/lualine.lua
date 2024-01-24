045a738391c277edb76d6833c4a09c99322fb324
JZ Loh <ljzyx86@qq.com>
2024-01-24 12:51:27 +0800

    remove diagnostics in status line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,6 +2,7 @@
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
+      table.remove(opts.sections.lualine_c)
       table.remove(opts.sections.lualine_z)
     end,
   }
