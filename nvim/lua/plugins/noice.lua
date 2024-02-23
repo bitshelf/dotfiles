f21f1582d953b8dad742425ad2463204ec36dfa7
JZ Loh <ljzyx86@qq.com>
2024-02-23 15:09:54 +0800

    update noice config

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -2,22 +2,47 @@ return {
   {
     "folke/noice.nvim",
     -- event = "VeryLazy",
+
+    dependencies = { -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim", -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify"},
+
     opts = {
       lsp = {
+		progress = {
+			enabled = true
+		},
+		
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
         },
       },
+
+	  messages = {
+		  -- NOTE: If you enable messages, then the cmdline is enabled automatically.
+		  -- This is a current Neovim limitation.
+		  enabled = true, -- enables the Noice messages UI
+		  view = "notify", -- default view for messages
+		  view_error = "notify", -- view for errors
+		  view_warn = "notify", -- view for warnings
+		  view_history = "messages", -- view for :messages
+		  view_search = "virtualtext" -- view for search count messages. Set to `false` to disable
+	  },
+
       routes = {
         {
           filter = {
-            event = "msg_show",
+            event = { "msg_showmode", "msg_showcmd", "msg_ruler", "msg_show" },
             any = {
               { find = "%d+L, %d+B" },
               { find = "; after #%d+" },
               { find = "; before #%d+" },
+              { find = "Ignoring" },
+              { find = "lines" },
             },
           },
 		  opts = { skip = true },
@@ -32,11 +57,10 @@ return {
 		  opts = { skip = true },
 		},
 
-		{
-		  view = "notify",
-		  filter = { event = "msg_showmode" },
-		  opts = { skip = true },
-		},
+		-- {
+		--   view = "notify",
+		--   filter = { event = "msg_showmode" },
+		-- },
 
 		{
 		  view = "notify",
@@ -47,12 +71,71 @@ return {
 		  opts = { skip = true },
 		},
       },
+
+	  cmdline = {
+		  enabled = true, -- enables the Noice cmdline UI
+		  view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+		  opts = {}, -- global options for the cmdline. See section on views
+		  ---@type table<string, CmdlineFormat>
+		  format = {
+			  -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+			  -- view: (default is cmdline view)
+			  -- opts: any options passed to the view
+			  -- icon_hl_group: optional hl_group for the icon
+			  -- title: set to anything or empty string to hide
+			  cmdline = {
+				  pattern = "^:",
+				  icon = "",
+				  lang = "vim"
+			  },
+			  search_down = {
+				  kind = "search",
+				  pattern = "^/",
+				  icon = " ",
+				  lang = "regex"
+			  },
+			  search_up = {
+				  kind = "search",
+				  pattern = "^%?",
+				  icon = " ",
+				  lang = "regex"
+			  },
+			  filter = {
+				  pattern = "^:%s*!",
+				  icon = "$",
+				  lang = "bash"
+			  },
+			  lua = {
+				  pattern = {"^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*"},
+				  icon = "",
+				  lang = "lua"
+			  },
+			  help = {
+				  pattern = "^:%s*he?l?p?%s+",
+				  icon = ""
+			  },
+			  input = {} -- Used by input()
+			  -- lua = false, -- to disable a format, set to `false`
+		  }
+	  },
+
       presets = {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
         inc_rename = true,
       },
+
+	  notify = {
+		  -- Noice can be used as `vim.notify` so you can route any notification like other messages
+		  -- Notification messages have their level and other properties set.
+		  -- event is always "notify" and kind can be any log level as a string
+		  -- The default routes will forward notifications to nvim-notify
+		  -- Benefit of using Noice for this is the routing and consistent history view
+		  enabled = false,
+		  view = "notify"
+	  },
+
     },
     -- stylua: ignore
     keys = {
