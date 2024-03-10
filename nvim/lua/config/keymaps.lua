4e012d6b02fefebd8adcb1d4a85f72ff84e01cf5
JZ Loh <luojianzhi@industio.com>
2024-03-10 14:34:15 +0800

    update comment keybind to <c-/>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,10 +1,5 @@
-local opts = { noremap = true, silent = true, nowait = true }
+local opts = { noremap = true, silent = true, nowait = true, desc = "Go to left window" }
 -- local keymap = vim.keymap.set
-
-vim.keymap.set('n', "<F2>", ":nohlsearch<CR>", { silent = true, desc = "nohlsearch" })
-vim.keymap.set({'n','x'}, "ZQ", "<cmd>qa!<CR>", { silent = true, desc = "visual quit not save" })
-vim.keymap.set({'n','x'}, "ZZ", "<cmd>wqa<CR>", { silent = true ,desc = "visual quit" })
-vim.keymap.set('n', '<leader>l', "<cmd>UndotreeToggle<cr>", { desc = "UndotreeToggle", silent = true, nowait = true })
 
 -- vim.keymap.del('n',"<leader>e")
 vim.keymap.del('n', "<leader>L")
@@ -12,15 +7,28 @@ vim.keymap.del('n', "<leader>l")
 vim.keymap.del('n', "<c-/>")
 vim.keymap.del('n', "<c-_>")
 
-local api = require("Comment.api")
-vim.keymap.set("n", "<c-/>", api.locked("comment.linewise.current"))
-vim.keymap.set("n", "<c-/>", api.locked("uncomment.linewise.current"))
-vim.keymap.set("n", "<c-_>", api.locked("comment.linewise.current"))
-vim.keymap.set("n", "<c-_>", api.locked("uncomment.linewise.current"))
+vim.keymap.set('n', "<F2>", ":nohlsearch<CR>", { silent = true, desc = "nohlsearch" })
+vim.keymap.set({'n','x'}, "ZQ", "<cmd>qa!<CR>", { silent = true, desc = "visual quit not save" })
+vim.keymap.set({'n','x'}, "ZZ", "<cmd>wqa<CR>", { silent = true ,desc = "visual quit" })
+vim.keymap.set('n', '<leader>l', "<cmd>UndotreeToggle<cr>", { desc = "UndotreeToggle", silent = true, nowait = true })
+
+-- vim.api.nvim_set_keymap("n", "<C-_>", "<Cmd>lua require('Comment.api').toggle.linewise.current()<CR>", {})
+-- vim.api.nvim_set_keymap("i", "<C-_>", "<Esc>:<C-u>lua require('Comment.api').toggle.linewise.current()<CR>\"_cc", {})
+-- vim.api.nvim_set_keymap("v", "<C-_>", "gc", {})
+-- vim.api.nvim_set_keymap("n", "<C-/>", "<Cmd>lua require('Comment.api').toggle.linewise.current()<CR>", {})
+-- vim.api.nvim_set_keymap("i", "<C-/>", "<Esc>:<C-u>lua require('Comment.api').toggle.linewise.current()<CR>\"_cc", {})
+-- vim.api.nvim_set_keymap("v", "<C-/>", "gc", {})
+vim.keymap.set("n", "<c-/>", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", { noremap = true, silent = true, nowait = true, desc = "Toggle Comment line" })
+vim.keymap.set("x", "<c-/>", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { noremap = true, silent = true, nowait = true, desc = "Toggle Comment visualmode" })
+vim.keymap.set("n", "<c-_>", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", { noremap = true, silent = true, nowait = true, desc = "Toggle Comment line" })
+vim.keymap.set("x", "<c-_>", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { noremap = true, silent = true, nowait = true, desc = "Toggle Comment visualmode" })
 
 -- remove lazyvim terminal keybind
-vim.keymap.set("t", "<c-h>", "<c-h>", { buffer = buf, nowait = true })
-vim.keymap.set("t", "<c-j>", "<c-j>", { buffer = buf, nowait = true })
-vim.keymap.set("t", "<c-k>", "<c-k>", { buffer = buf, nowait = true })
-vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = buf, nowait = true })
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]],{ buffer = buf, nowait = true })
+-- vim.keymap.set("t", "<c-h>", "<c-h>", { buffer = buf, nowait = true })
+-- vim.keymap.set("t", "<c-j>", "<c-j>", { buffer = buf, nowait = true })
+-- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]],{ buffer = buf, nowait = true })
+vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+vim.keymap.set("t", "<c-k>", "<c-k>", { buffer = buf, nowait = true, desc = "delte after chars" })
+vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = buf, nowait = true, desc = "Clear screen" })
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
