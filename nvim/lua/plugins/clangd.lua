ad7d27ac4f64aac9f1207aa3a0c462c9cf649e19
JZ Loh <ljzyx86@qq.com>
2024-05-30 14:25:26 +0800

    update clangd config

--- nvim/lua/plugins/clangd.lua
+++ nvim/lua/plugins/clangd.lua
@@ -11,11 +11,9 @@ return {
 					-- "--clang-tidy",
 					"--header-insertion=iwyu",
 					"--completion-style=detailed",
-					"--function-arg-placeholders",
 					"--fallback-style=llvm",
 					"--header-insertion=never",
 					"--all-scopes-completion",
-					"--completion-style=detailed",
 					"--offset-encoding=utf-16",
 					"--pch-storage=memory",
 					-- 启用这项时，补全函数时，将会给参数提供占位符，键入后按 Tab 可以切换到下一占位符
