833787fb21cf9e20a9c3269047daac3e5a309d28
JZ Loh <ljzyx86@qq.com>
2024-07-11 22:39:45 -0700

    update nvm install method

--- hostConfig/install.sh
+++ hostConfig/install.sh
@@ -14,7 +14,8 @@ function install_node {
 	mkdir -p $HOME/.local/.nvm/
 	export NVM_DIR="$HOME/.local/.nvm"
 	# wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+	# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+	export NVM_DIR="$HOME/.nvm" && ( git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR";   cd "$NVM_DIR";   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`; ) && \. "$NVM_DIR/nvm.sh"
 
 	export NVM_DIR="$HOME/.local/.nvm"
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
