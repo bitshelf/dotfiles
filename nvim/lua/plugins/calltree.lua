56ce0cfb89b3db9534062154ccb624e5dee7fe97
JZ Loh <jz.luo@myir.com>
2024-08-06 14:45:38 +0800

    add plugin 'ldelossa/litee.nvim'

--- nvim/lua/plugins/calltree.lua
+++ nvim/lua/plugins/calltree.lua
@@ -1,5 +1,18 @@
 return {
   {
+    'ldelossa/litee.nvim',
+    event = "VeryLazy",
+    opts = {
+      notify = { enabled = false },
+      panel = {
+          orientation = "bottom",
+          panel_size = 10,
+      },
+    },
+    config = function(_, opts) require('litee.lib').setup(opts) end
+  },
+
+  {
     "ldelossa/litee-calltree.nvim",
 	ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
 	event = "LazyFile",
