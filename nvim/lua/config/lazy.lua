c60055306c1534a88beb2dc2785c69585764b29a
JZ Loh <ljzyx86@qq.com>
2024-08-23 16:40:51 +0800

    disabled copilot

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
 
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     -- { import = "lazyvim.plugins.extras.lang.java" },
     -- { import = "lazyvim.plugins.extras.lang.rust" },
