298f187ee7f9634b2fe92052c4d725589f2c72b4
JZ Loh <ljzyx86@qq.com>
2024-08-30 14:37:31 +0800

    nvim git plugin config

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,45 +1,76 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-		-- event = "VeryLazy",
+		enabled = vim.fn.executable "git" == 1,
 		event = "LazyFile",
+
+		opts = {
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+				untracked = { text = "▎" },
+			},
+
+			signs_staged = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+			},
+			signs_staged_enable = true,
+		},
+
 		config = function()
-			require('gitsigns').setup({
-				signs = {
-					add          = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-					change       = { hl = 'GitSignsChange', text = '░', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-					delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-					topdelete    = { hl = 'GitSignsDelete', text = '▔', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-					changedelete = { hl = 'GitSignsChange', text = '▒', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-					untracked    = { hl = 'GitSignsAdd', text = '┆', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-				},
-			})
-			vim.keymap.set("n", "<leader>g-", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>g=", ":Gitsigns next_hunk<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>gb", ":Gitsigns blame_line<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "H", ":Gitsigns preview_hunk_inline<CR>", { noremap = true, silent = true })
-		end
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-		keys = {"<leader>gg"},
-		-- enabled = false,
-		config = function()
-			vim.g.lazygit_floating_window_scaling_factor = 1.0
-			vim.g.lazygit_floating_window_winblend = 0
-			vim.g.lazygit_use_neovim_remote = true
-			vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "LazyGit", noremap = true, silent = true })
-		end
-	},
-	-- {
-	-- 	"APZelos/blamer.nvim",
-	-- 	config = function()
-	-- 		vim.g.blamer_enabled = true
-	-- 		vim.g.blamer_relative_time = true
-	-- 	end
-	-- }
+			require('gitsigns').setup{
+				on_attach = function(bufnr)
+					local gitsigns = require('gitsigns')
+
+					local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map('n', ']h', function()
+					if vim.wo.diff then
+						vim.cmd.normal({']h', bang = true})
+					else
+						gitsigns.nav_hunk('next')
+					end
+					end)
+
+					map('n', '[h', function()
+					if vim.wo.diff then
+						vim.cmd.normal({'[h', bang = true})
+					else
+						gitsigns.nav_hunk('prev')
+					end
+					end)
+
+					-- Actions
+					map('n', '<leader>hs', gitsigns.stage_hunk)
+					map('n', '<leader>hr', gitsigns.reset_hunk)
+					map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+					map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+					map('n', '<leader>hS', gitsigns.stage_buffer)
+					map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+					map('n', '<leader>hR', gitsigns.reset_buffer)
+					map('n', '<leader>hp', gitsigns.preview_hunk)
+					map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+					map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+					map('n', '<leader>hd', gitsigns.diffthis)
+					map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+					map('n', '<leader>td', gitsigns.toggle_deleted)
+
+					-- Text object
+					map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+				end
+			}
+		end,
+	}
 }
