632d383e68d577bacb6eb5febd50231db160a3aa
JZ Loh <ljzyx86@qq.com>
2024-10-06 11:19:02 +0800

    disabled lang provider

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,10 @@ vim.g.maplocalleader = "\\"
 vim.g.lazyvim_picker = "telescope"
 vim.g.root_spec = { "lsp", {".repo", ".git", "lua" }, "cwd" }
 vim.g.autoformat = false
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
 
 vim.o.termguicolors = true
 vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
