e48d30d66da157df0490935280f017b1863b21f3
JZ Loh <JZ.luo@myir.cn>
2025-03-06 20:42:56 +0800

    update to mamba

--- hostConfig/terminal.sh
+++ hostConfig/terminal.sh
@@ -153,19 +153,20 @@ rehash(){
 
 	# >>> conda initialize >>>
 	# !! Contents within this block are managed by 'conda init' !!
-	if [ -d "$HOME/miniconda3/bin" ] ; then
-		export PATH="$HOME/miniconda3//bin/:$PATH"
-	fi
-	__conda_setup="$('$HOME/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+	__conda_setup="$('$HOME/.miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 	if [ $? -eq 0 ]; then
 		eval "$__conda_setup"
 	else
-		if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
-			. "$HOME/miniconda3/etc/profile.d/conda.sh"
+		if [ -f "$HOME/.miniforge3/etc/profile.d/conda.sh" ]; then
+			. "$HOME/.miniforge3/etc/profile.d/conda.sh"
 		else
-			export PATH="$HOME/miniconda3/bin:$PATH"
+			export PATH="$HOME/.miniforge3/bin:$PATH"
 		fi
 	fi
 	unset __conda_setup
+
+	if [ -f "$HOME/.miniforge3/etc/profile.d/mamba.sh" ]; then
+		. "$HOME/.miniforge3/etc/profile.d/mamba.sh"
+	fi
 	# <<< conda initialize <<<
 }
