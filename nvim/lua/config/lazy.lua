a8e36f8b20ce2d360d9ee07a911984929b6b6f81
JZ Loh <JZ.luo@myir.cn>
2025-04-30 09:56:47 +0800

    update lazyvim setting

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,27 +15,22 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
     -- import any extras modules here
-	-- { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    -- { import = "lazyvim.plugins.extras.lang.java" },
+    -- { import = "lazyvim.plugins.extras.lang.rust" },
+
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    -- { import = "lazyvim.plugins.extras.coding.luasnip" },
 	{ import = "lazyvim.plugins.extras.editor.aerial" },
 	{ import = "lazyvim.plugins.extras.ui.treesitter-context" },
 	{ import = "lazyvim.plugins.extras.ui.mini-indentscope" },
-    -- { import = "lazyvim.plugins.extras.lang.java" },
-    -- { import = "lazyvim.plugins.extras.lang.rust" },
+	-- { import = "lazyvim.plugins.extras.editor.telescope" },
+    -- { import = "lazyvim.plugins.extras.coding.luasnip" },
 
     -- import/override with your plugins
     { import = "plugins" },
@@ -52,18 +47,15 @@ require("lazy").setup({
     },
   },
 
-  -- rocks = {
-  --   enabled = false,
-  --   root = vim.fn.stdpath("data") .. "/lazy-rocks",
-  --   server = "https://nvim-neorocks.github.io/rocks-binaries/",
-  -- },
-
   defaults = {
     version = "*", -- try installing the latest stable version for plugins that support semver
   },
 
   install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true },
+  checker = {
+		enabled = true,
+		notify = false,
+	},
 
  --  ui = {
 	-- icons = {
