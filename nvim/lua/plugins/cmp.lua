d8b8aab2f2e229004c62a5ac26992b45a732d36e
JZ Loh <ljzyx86@qq.com>
2025-05-11 22:32:14 +0800

    modify blink config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,17 +1,49 @@
 return {
   'saghen/blink.cmp',
+  event = {'BufReadPost', 'BufNewFile' },
+  dependencies = { "xzbdmw/colorful-menu.nvim", opts ={} },
 
   opts = {
-    keymap = { 
+    keymap = {
 		preset = 'super-tab',
 
 		['<CR>'] = { 'accept', 'fallback' },
 		['<C-y>'] = { 'select_and_accept' },
+		['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+		['<C-d>'] = { 'scroll_documentation_down', 'fallback' }
 	},
 
     completion = {
 		ghost_text = {
 			enabled = false,
+		},
+		documentation = {
+				auto_show = true,
+		},
+		menu = {
+			draw = {
+				columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+				components = {
+					label = {
+						text = function(ctx)
+							return require('colorful-menu').blink_components_text(ctx)
+						end,
+						highlight = function(ctx)
+							return require('colorful-menu').blink_components_highlight(ctx)
+						end,
+					},
+				},
+			},
+		},
+    },
+	signature = {
+		enabled = true,
+	},
+	cmdline = {
+		completion = {
+			menu = {
+				auto_show = true,
+			},
 		},
     },
   },
