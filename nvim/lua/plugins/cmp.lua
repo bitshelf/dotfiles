d5d15dfc7cd9dea5f1a77765a4af8abe7d372ac1
JZ Loh <JZ.luo@myir.cn>
2025-07-09 14:16:26 +0800

    udpate keybind for blink.cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -5,12 +5,34 @@ return {
 
   opts = {
     keymap = {
-		preset = 'super-tab',
-
 		['<CR>'] = { 'accept', 'fallback' },
 		['<C-y>'] = { 'select_and_accept' },
 		['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-		['<C-d>'] = { 'scroll_documentation_down', 'fallback' }
+		['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+
+		-- super-tab
+		['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+		['<C-e>'] = { 'hide', 'fallback' },
+
+		['<Tab>'] = {
+		function(cmp)
+			if cmp.snippet_active() then return cmp.accept()
+			else return cmp.select_and_accept() end
+		end,
+		'snippet_forward',
+		'fallback'
+		},
+		['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+		['<Up>'] = { 'select_prev', 'fallback' },
+		['<Down>'] = { 'select_next', 'fallback' },
+		['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+		['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+		['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+		['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+		['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
 	},
 
     completion = {
@@ -18,7 +40,7 @@ return {
 			enabled = false,
 		},
 		documentation = {
-				auto_show = true,
+			auto_show = true,
 		},
 		menu = {
 			draw = {
